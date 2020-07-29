62f6f368b8cf9ea80582334d6c69963219a1092b
marscj <mjx_cj@163.com>
2020-07-29 13:16:57 +0400

    commit

---
+++ lib/router/router.gr.dart
@@ -0,0 +1,95 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../pages/pages.dart';
+
+class Routes {
+  static const String authentication = '/';
+  static const String _userPage = '/users/:id';
+  static String userPage({@required dynamic id}) => '/users/$id';
+  static const all = <String>{
+    authentication,
+    _userPage,
+  };
+}
+
+class Router extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(Routes.authentication, page: Authentication),
+    RouteDef(
+      Routes._userPage,
+      page: UserPage,
+      generator: UserPageRouter(),
+    ),
+  ];
+  @override
+  Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, AutoRouteFactory>{
+    Authentication: (data) {
+      return CupertinoPageRoute<dynamic>(
+        builder: (context) => Authentication(),
+        settings: data,
+      );
+    },
+    UserPage: (data) {
+      return CupertinoPageRoute<Widget>(
+        builder: (context) => UserPage(),
+        settings: data,
+      );
+    },
+  };
+}
+
+class UserPageRoutes {
+  static const String userProfilePage = '/';
+  static const String userPhotoPage = '/photo';
+  static const String userPostPage = '/post';
+  static const all = <String>{
+    userProfilePage,
+    userPhotoPage,
+    userPostPage,
+  };
+}
+
+class UserPageRouter extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(UserPageRoutes.userProfilePage, page: UserProfilePage),
+    RouteDef(UserPageRoutes.userPhotoPage, page: UserPhotoPage),
+    RouteDef(UserPageRoutes.userPostPage, page: UserPostPage),
+  ];
+  @override
+  Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, AutoRouteFactory>{
+    UserProfilePage: (data) {
+      return CupertinoPageRoute<dynamic>(
+        builder: (context) => UserProfilePage(),
+        settings: data,
+      );
+    },
+    UserPhotoPage: (data) {
+      return CupertinoPageRoute<dynamic>(
+        builder: (context) => UserPhotoPage(),
+        settings: data,
+      );
+    },
+    UserPostPage: (data) {
+      return CupertinoPageRoute<dynamic>(
+        builder: (context) => UserPostPage(),
+        settings: data,
+      );
+    },
+  };
+}
